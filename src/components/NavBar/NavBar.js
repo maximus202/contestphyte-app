02eafdf39df051eea6eaaf021d3686f9ea7ba6c5
Max R <maxx.ratto@gmail.com>
2020-05-14 16:42:22 -0600

    Added menu icon image in nav.

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -7,7 +7,7 @@ function NavBar() {
       <a href="/">
         <img src="https://github.com/maximus202/contestphyte-app/blob/223d1478b020322c06713c913fb4d7cee41a010c/public/contestphytelogo.png?raw=true" alt="Contestphyte logo." />
       </a>
-      <p>Menu</p>
+      <img src="https://github.com/maximus202/contestphyte-app/blob/master/public/menu.png?raw=true" alt="Menu." className="menu-icon" />
     </nav>
   );
 }
